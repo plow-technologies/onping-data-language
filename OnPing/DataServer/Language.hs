cde9e79d898e6396f7ec15470759768bd6fddf7e
Daniel Díaz <dhelta.diaz@gmail.com>
2015-09-17 06:39:39 +0200

    Now with the right capitalization.

--- OnPing/DataServer/Language.hs
+++ OnPing/DataServer/Language.hs
@@ -454,7 +454,7 @@ prelude = do
   assign "display" (pack . displayValue :: Value -> Text)
   -- Keys
   assign "Key" Key
-  assign "keypid" keypid
+  assign "keypid" keyPID
   -- Server default info
   assign "server" ("127.0.0.1" :: Text)
   assign "port" (5000 :: Int)
